fc6679ce7f7d3e935fce5cf0f12a8934223571ca
apeszek <Aria@Aria-Pro.MG8702>
2026-01-11 23:20:42 -0800

    first 3

--- asgn0/asgn0.js
+++ asgn0/asgn0.js
@@ -11,6 +11,50 @@ function main() {
   var ctx = canvas.getContext('2d');
 
   // Draw a blue rectangle
-  ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set color to blue
-  ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
+  ctx.fillStyle = "black"; //Sets to black
+  ctx.fillRect(0, 0, canvas.width, canvas.height); // Fill a rectangle with the color
+
+  //creates vector v1
+  let v1 = new Vector3([2.25, 2.25, 0]);
+  let v2 = new Vector3([7.00, 2.25, 0]);
+  //calls drawVector function
+  drawVector(ctx, v1, "red");
+  drawVector(ctx, v2, "blue");
+
 }
+
+//FUNCTIONS
+//creates drawVector function 
+  function drawVector(ctx, v, color){
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 2;
+    let X = 200;
+    let Y = 200;
+    let scale = 20;
+
+    ctx.beginPath();
+    ctx.moveTo(X, Y);
+    ctx.lineTo(X + v.elements[0] * scale, Y - v.elements[1] * scale);
+    ctx.stroke();
+  }
+
+  //creates drawButton function
+  //called when button is clicked
+  function handleDrawEvent(){
+    var canvas = document.getElementById('example');
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = "black"; //Sets to black
+    ctx.fillRect(0, 0, canvas.width, canvas.height); // Fill a rectangle with the color
+
+    let x = parseFloat(document.getElementById("xInput").value);
+    let y = parseFloat(document.getElementById("yInput").value);
+
+    let v1 = new Vector3([x, y, 0]);
+    drawVector(ctx, v1, "red");
+
+    let x2 = parseFloat(document.getElementById("x2Input").value);
+    let y2 = parseFloat(document.getElementById("y2Input").value);
+
+    let v2 = new Vector3([x2, y2, 0]);
+    drawVector(ctx, v2, "blue");
+  }
