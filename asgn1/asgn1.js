48699e835c50c4f2c4f25cbbf1893e8acaa76ad9
apeszek <apeszek@ucsc.edu>
2026-01-14 17:40:39 -0800

    part 2

--- asgn1/asgn1.js
+++ asgn1/asgn1.js
@@ -1,48 +1,3 @@
-// HelloPoint1.js (c) 2012 matsuda
-// Vertex shader program
-var VSHADER_SOURCE = 
-  'void main() {\n' +
-  '  gl_Position = vec4(0.0, 0.0, 0.0, 1.0);\n' + // Set the vertex coordinates of the point
-  '  gl_PointSize = 10.0;\n' +                    // Set the point size
-  '}\n';
-
-// Fragment shader program
-var FSHADER_SOURCE =
-  'void main() {\n' +
-  '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' + // Set the point color
-  '}\n';
-
-function main() {
-  // Retrieve <canvas> element
-  var canvas = document.getElementById('webgl');
-
-  // Get the rendering context for WebGL
-  var gl = getWebGLContext(canvas);
-  if (!gl) {
-    console.log('Failed to get the rendering context for WebGL');
-    return;
-  }
-
-  // Initialize shaders
-  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-    console.log('Failed to intialize shaders.');
-    return;
-  }
-
-  // Specify the color for clearing <canvas>
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
-  // Clear <canvas>
-  gl.clear(gl.COLOR_BUFFER_BIT);
-
-  // Draw a point
-  gl.drawArrays(gl.POINTS, 0, 1);
-}
-
-
-
-
-/*
 //COLOREDPOINT.JS
 var VSHADER_SOURCE =
   'attribute vec4 a_Position;\n' +
@@ -137,4 +92,3 @@ function click(ev, gl, canvas, a_Position, u_FragColor) {
     gl.drawArrays(gl.POINTS, 0, 1);
   }
 }
-*/
