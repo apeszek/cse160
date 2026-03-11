a0a581fb3be2917a8c9aa485c96727564ed76cf6
apeszek <apeszek@ucsc.edu>
2026-03-10 23:44:12 -0700

    skyCube and car base

--- asgn5/src/main.js
+++ asgn5/src/main.js
@@ -41,9 +41,9 @@ function main() {
   const fov = 75;
   const aspect = window.innerWidth / window.innerHeight;
   const near = 0.1;
-  const far = 100;
+  const far = 1000;
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(0, 5, 0);
+  camera.position.set(0, 3, 10);
   camera.lookAt(0, 0, 0);
 
   window.addEventListener('resize', () => {
@@ -61,15 +61,29 @@ function main() {
   //scene.add(cube);
 
   //plane (road)
-  const planeGeometry = new THREE.PlaneGeometry(50, 3);  // wide left-right, narrow depth
+  const planeGeometry = new THREE.PlaneGeometry(50, 8);  // wide left-right, narrow depth
   const planeMaterial = new THREE.MeshPhongMaterial({color: 0x888888});  // road gray
   const plane = new THREE.Mesh(planeGeometry, planeMaterial);
   plane.rotation.x = -Math.PI / 2;  // lay flat
   plane.position.y = -2;            // push below the cubes
-  plane.position.z = 4;
+  plane.position.z = 6;
   scene.add(plane);
 
-  //const car = new THREE.Object3D();
+  //car
+  const objects = []; //array to hold objects that will rotate
+  const car = new THREE.Object3D();
+  scene.add(car);
+  //objects.push(car);
+
+  const carMaterial = new THREE.MeshPhongMaterial({emissive: 0xBF0A30});
+  const baseCarGeometry = new THREE.BoxGeometry(1.5, 0.5, 1);
+  const baseCarMesh = new THREE.Mesh(baseCarGeometry, carMaterial);
+  baseCarMesh.scale.set(3.5,3.5,3.5);
+  baseCarMesh.position.x = 1.5;
+  baseCarMesh.position.z = 2;
+  baseCarMesh.position.y = -0.5;
+  car.add(baseCarMesh);
+  //objects.push(baseCarMesh);
 
 
 
@@ -98,7 +112,16 @@ function main() {
   makeCubeInstance(cubeGeometry, 0x8844aa, -2),
   makeCubeInstance(cubeGeometry, 0xaa8844,  2),
     ];
-    */
+*/
+
+  //skybox
+  const loader = new THREE.TextureLoader();
+  const skyTexture = loader.load('blue-sky.jpg');
+  skyTexture.colorSpace = THREE.SRGBColorSpace;
+  const skyGeo = new THREE.BoxGeometry(1000, 1000, 1000);
+  const skyMat = new THREE.MeshBasicMaterial({ map: skyTexture, side: THREE.BackSide });
+  const skyBox = new THREE.Mesh(skyGeo, skyMat);
+  scene.add(skyBox);
 
 
 //function to animate
